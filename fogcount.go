d9c3dc08b98a6a4bf18026c400f4d806b25169ac
Carlos C <uldericofilho@gmail.com>
2016-06-30 17:29:11 -0700

    add package description

--- fogcount.go
+++ fogcount.go
@@ -1,3 +1,15 @@
+// Gunning-fog index analyzer written in Go. This analyzer processes an English
+// text and produces its Gunning Fox index score. Refer to its logic in
+// https://en.wikipedia.org/wiki/Gunning_fog_index - it does not analyse word
+// endings (-es, -ed, or -ing), or discriminate proper nouns, familiar jargon or
+// compound words.
+//
+//
+//     $ go get cirello.io/gunning-fog/...
+//     $ cat LICENSE | $GOPATH/bin/gunning-fog
+//     16
+//
+// `gunning-fog` will always wait content from STDIN.
 package main // import "cirello.io/gunning-fog"
 
 import (
