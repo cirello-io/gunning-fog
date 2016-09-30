836c52ae814c0335374ccc8cd80f73317ea10448
Carlos C <uldericofilho@gmail.com>
2016-10-01 00:11:25 +0200

    Code grooming
    Fix misspell and golint warnings.

--- fogcount/fg.go
+++ fogcount/fg.go
@@ -5,7 +5,7 @@ import (
 	"io"
 )
 
-// Analyzer processes an English text and produces its Gunning Fox index score.
+// Analyze processes an English text and produces its Gunning Fox index score.
 // Refer to its logic in https://en.wikipedia.org/wiki/Gunning_fog_index - it
 // does not analyse word endings (-es, -ed, or -ing), or discriminate proper
 // nouns, familiar jargon or compound words.
